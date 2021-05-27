1aeebc23f79b129192b0f5e132a6ae44fc20e563
Tony <tonyyu1129@gmail.com>
2021-05-27 09:57:22 -0700

    Updates

--- precache-manifest.d0715089a9adecd2c795249c40956281.js
+++ precache-manifest.d0715089a9adecd2c795249c40956281.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cd8340f2f5f75fb79deb1b13329edf99",
+    "revision": "98b8bac7af67b380c8b0cd7786248034",
     "url": "/fish-countdown/index.html"
   },
   {
-    "revision": "85cf85f30a2441b57ef8",
-    "url": "/fish-countdown/static/css/main.ea950b93.chunk.css"
+    "revision": "e2d72fae8f77067887d7",
+    "url": "/fish-countdown/static/css/main.9be647ae.chunk.css"
   },
   {
     "revision": "883c32ffe4492180e299",
@@ -16,15 +16,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fish-countdown/static/js/2.b45ee002.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "85cf85f30a2441b57ef8",
-    "url": "/fish-countdown/static/js/main.c8b772cc.chunk.js"
+    "revision": "e2d72fae8f77067887d7",
+    "url": "/fish-countdown/static/js/main.61cd6618.chunk.js"
   },
   {
     "revision": "ee2aaa9b7aea3f4753ab",
     "url": "/fish-countdown/static/js/runtime-main.affa47a4.js"
-  },
-  {
-    "revision": "575932561ad5de9cc002e6a032fd1881",
-    "url": "/fish-countdown/static/media/tony-coffee.57593256.jpeg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fish-countdown/precache-manifest.60640d299b9d4193135ef2859d65b500.js"
+  "/fish-countdown/precache-manifest.d0715089a9adecd2c795249c40956281.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fish-countdown/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
